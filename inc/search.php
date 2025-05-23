a46ea51f4b6727cea2761b679a3c30c7efe5ef8d
Prabhu Maniraj <prabhu.kumarm@gmail.com>
2025-05-23 13:47:56 +0100

    Update search.php
    Removed redundant session variable check

--- inc/search.php
+++ inc/search.php
@@ -1,10 +1,10 @@
 <?php
-session_start();
+// session_start();
 
-if (!isset($_SESSION['user_id'])) {
-    header("Location: index.html");
-    exit();
-}
+// if (!isset($_SESSION['user_id'])) {
+//     header("Location: index.html");
+//     exit();
+// }
 
 
 include '../inc/db.php';
